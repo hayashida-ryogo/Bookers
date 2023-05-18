83f804f79a1ee8b917afeac9dbbe9b33a0f6efc1
hayashida-ryogo <hayashidaryogo@gmail.com>
2023-05-18 10:39:29 +0000

    [Create] top-page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,9 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+  end
+
   def index
   end
 
@@ -9,6 +12,12 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
 end
